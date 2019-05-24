647da7f2afb5a0ad0ae5740863b8acff7a3991a9
Elisabetta <elisabetta.piombin@gmail.com>
2019-05-24 09:28:42 +0200

    added verofalso file

---
+++ verofalso.cpp
@@ -0,0 +1,9 @@
+//
+//  verofalso.cpp
+//  exam-checker
+//
+//  Created by Elisabetta Piombin on 24/05/2019.
+//  Copyright © 2019 Elisabetta Piombin. All rights reserved.
+//
+
+#include "verofalso.hpp"
---
+++ verofalso.hpp
@@ -0,0 +1,13 @@
+//
+//  verofalso.hpp
+//  exam-checker
+//
+//  Created by Elisabetta Piombin on 24/05/2019.
+//  Copyright © 2019 Elisabetta Piombin. All rights reserved.
+//
+
+#ifndef verofalso_hpp
+#define verofalso_hpp
+
+
+#endif /* verofalso_hpp */
